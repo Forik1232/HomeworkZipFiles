cf7c3b6a04a6f98a853706c4b01ce19dd18f43d9
Daniil <9414354@mail.ru>
2022-04-26 09:57:34 +0300

    .added new variant

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -20,9 +20,10 @@ import java.util.zip.ZipInputStream;
 import static com.codeborne.selenide.Selenide.$;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static com.codeborne.selenide.Selenide.open;
 
 
-public class SelenideFilesTest {
+/*public class SelenideFilesTest {
     ClassLoader classLoader = getClass().getClassLoader();
     ClassLoader cl = SelenideFilesTest.class.getClassLoader();
 
@@ -50,19 +51,55 @@ public class SelenideFilesTest {
         }
 
     }
-}
 
-    @Test
+
+   @Test
     void exelTest() throws Exception{
         try (InputStream is = classLoader.getResourceAsStream("resources/ExelTest.xls.zip");
-             XLS xls = new XLS(is);
+             XLS xls = new XLS(is)) {
              assertThat(xls.excel
                      .getSheet(0)
                      .getRow(1)
                      .getCell(1)
                      .getStringCellValue()).contains("так");
+
+    }
 }
 }
+*/
+
+public class SelenideFilesTest {
+    ClassLoader classLoader = getClass().getClassLoader();
+    ClassLoader slob = SelenideFilesTest.class.getClassLoader();
+    ClassLoader cl = SelenideFilesTest.class.getClassLoader();
+
+
+    @Test
+    void testPdfFiles() throws Exception {
+        InputStream stream = slob.getResourceAsStream("PdfTest.pdf");
+        PDF pdf = new PDF(stream);
+        Assertions.assertEquals(2, pdf.numberOfPages);
+
+    }
+
+
+    @Test
+    void csvParsingTest() throws Exception {
+        try (InputStream stream = cl.getResourceAsStream("CSVTest.csv.zip");
+             CSVReader reader = new CSVReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+            List<String[]> content = reader.readAll();
+            org.assertj.core.api.Assertions.assertThat(content).contains(
+            assertThat(content.get(0)).contains("Series_reference", "Period", "Data_value", "Suppressed", "STATUS",
+                    "UNITS", "Magnitude", "Subject", "Group", "Series_title_1", "Series_title_2", "Series_title_3",
+                    "Series_title_4", "Series_title_5");
+        }
+    }
+}
+
+
+
+
+
 
 
 
